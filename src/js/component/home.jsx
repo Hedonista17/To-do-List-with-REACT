1bc6da76361fc75a576775d5d5f516e063bef259
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-04 08:12:16 +0000

    primer intento

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const[inputValue,setInputValue] = useState("");
+	const[tareas,setTareas] = useState([]);
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="my-5"> ⭐ To Do List with React ⭐</h1>
+			<ul className="list-group mt-5">
+				<li> <input onChange = {(element) => setInputValue(element.target.value)} value = {inputValue} onKeyPress= type="text" className="form-control" placeholder="Escriba la tarea por realizar"/></li>
+				<li className="list-group-item">A second item  <FontAwesomeIcon icon={ faTrash } /></li>
+				<li className="list-group-item">A third item    <FontAwesomeIcon icon={ faTrash } /></li>
+				<li className="list-group-item">A fourth item     <FontAwesomeIcon icon={ faTrash } /></li>
+				<li className="list-group-item">And a fifth one    <FontAwesomeIcon icon={ faTrash } /></li>
+			</ul>
+      <div className="mx-3">23 tareas por realizar</div>
 		</div>
 	);
 };
