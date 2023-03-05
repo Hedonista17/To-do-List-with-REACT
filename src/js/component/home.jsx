f905c84361793ee901fab5f17e6f2a498ecb340f
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-05 16:48:33 +0000

    proyecto finalizado en teoria

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,27 +1,53 @@
-import React, {useState} from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+
 
 //include images into your bundle
 
 
 //create your first component
 const Home = () => {
-	const[inputValue,setInputValue] = useState("");
-	const[tareas,setTareas] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const [tareas, setTareas] = useState([]);
 	return (
 		<div className="container">
 			<h1 className="my-5"> ⭐ To Do List with React ⭐</h1>
-			<ul className="list-group mt-5">
-				<li> <input onChange = {(element) => setInputValue(element.target.value)} value = {inputValue} onKeyPress= type="text" className="form-control" placeholder="Escriba la tarea por realizar"/></li>
-				<li className="list-group-item">A second item  <FontAwesomeIcon icon={ faTrash } /></li>
-				<li className="list-group-item">A third item    <FontAwesomeIcon icon={ faTrash } /></li>
-				<li className="list-group-item">A fourth item     <FontAwesomeIcon icon={ faTrash } /></li>
-				<li className="list-group-item">And a fifth one    <FontAwesomeIcon icon={ faTrash } /></li>
+			<ul className="list-group">
+				<li className="list-group-item">
+
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)} 
+						value={inputValue} 
+						onKeyPress={(e) => {if(e.key === "Enter"){
+							setTareas(tareas.concat(inputValue))
+						}}}
+						placeholder="Escriba tarea a realizar"
+					></input>
+				</li>
+				{tareas.map((tarea, index) => ( //recorre el array de tareas para pintar cada una dentro de un li
+		               
+					<li className="list-group-item ">
+						{tarea}
+						<div className="text-end mx-0">
+						<i
+							className="fa-regular fa-trash-can fa-xl" 
+							onClick={() =>
+								setTareas(
+									tareas.filter((tarea, indiceActual) => index != indiceActual)
+								)
+							} 
+
+						></i>
+						</div>
+					</li>
+					
+				))}
 			</ul>
-      <div className="mx-3">23 tareas por realizar</div>
+
+			<div className="mx-3" id="contador"> {tareas.length}  tareas por realizar 😫</div>
 		</div>
 	);
 };
 
 export default Home;
+
