bffeb4b46a13531f1e50d76998e18420ea8fb5cd
Hedonista17 <andreaynereapiza@gmail.com>
2023-03-07 20:47:42 +0000

    realizar tareas añadidas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { container } from "webpack";
 
 
 //include images into your bundle
@@ -6,46 +7,74 @@ import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
+	const [inputValue, setInputValue] = useState();
 	const [tareas, setTareas] = useState([]);
+	const [realizadas, setRealizadas] = useState([]);
+
+	useEffect(() => {
+		console.log(realizadas)
+	}, [realizadas])
 	return (
 		<div className="container">
-			<h1 className="my-5"> ⭐ To Do List with React ⭐</h1>
-			<ul className="list-group">
-				<li className="list-group-item">
+			<div className="row">
+				<div className="col-6">
+					<h1 className="my-5"> ⭐ To Do List with React ⭐</h1>
+					<ul className="list-group">
+						<li className="list-group-item col-6">
 
-					<input
-						type="text"
-						onChange={(e) => setInputValue(e.target.value)} 
-						value={inputValue} 
-						onKeyPress={(e) => {if(e.key === "Enter"){
-							setTareas(tareas.concat(inputValue))
-							setInputValue(" ")} }}
-						placeholder="Escriba tarea a realizar"
-					></input>
-				</li>
-				{tareas.map((tarea, index) => ( //recorre el array de tareas para pintar cada una dentro de un li
-		               
-					<li className="list-group-item ">
-						{tarea}
-						<div className="text-end mx-0">
-						<i
-							className="fa-regular fa-trash-can fa-xl" 
-							onClick={() =>
-								setTareas(
-									tareas.filter((tarea, indiceActual) => index != indiceActual)
-								)
-							} 
+							<input placeholder="Escriba tarea a realizar" id="input"
+								type="text"
+								onChange={(e) => setInputValue(e.target.value)}
+								value={inputValue}
+								onKeyDown={(e) => {
+									if (e.key === "Enter" && document.getElementById("input").value !== "") {
+										setTareas(tareas.concat(inputValue))
+										setInputValue(" ")
+									}
+								}}
+								
+							></input>
+						</li>
+						{tareas.map((tarea, index) => (
+							<li className="list-group-item">
+								{tarea}
 
-						></i>
-						</div>
-					</li>
-					
-				))}
-			</ul>
+								<i class="fa-solid fa-check-to-slot fa-xl mx-4"
+									onClick={() => {
 
-			<div className="mx-3" id="contador"> { tareas.length == 1 ? tareas.length + " tarea por realizar 😫" :  tareas.length +" tareas por realizar 😫"} </div>
+										setRealizadas(realizadas.concat(tarea))
+										setTareas(tareas.filter((tarea, indiceActual) => index != indiceActual)
+										)
+									}}
+								></i>
+								<i
+									className="fa-regular fa-trash-can fa-xl"
+									onClick={() =>
+										setTareas(
+											tareas.filter((tarea, indiceActual) => index != indiceActual)
+										)
+									} ></i>
+							</li>
+						))}
+					</ul>
+
+					<div className="mx-3" id="contador"> {tareas.length == 1 ? tareas.length + " tarea por realizar 😫" : tareas.length + " tareas por realizar 😫"} </div>
+				</div>
+				<div className="col-6">
+					{realizadas.map((realizada) => {
+						return (
+							
+							<ul className="list-group">
+							
+							</ul>
+						)
+
+					}
+					)}
+				</div>
+			</div>
 		</div>
+
 	);
 };
 
